f4abc995d4c90fe7b9c1828f08d7cbf11c64ab97
caojianfei <120631902@qq.com>
2018-05-23 15:21:20 +0800

    文章回复表

---
+++ database/migrations/2018_05_23_150238_create_replays_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReplaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replays', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id', false, true)->index();
+            $table->integer('article_id', false, true)->index();
+            $table->integer('replay_id', false, true)->default(0)->index();
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replays');
+    }
+}
