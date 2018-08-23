2f11e1bf7cf253d3bcd8ee589c7504482c5760f4
caojianfei <120631902@qq.com>
2018-08-23 22:13:45 +0800

    删除评论时同步关联数据

--- app/Observers/ReplayObservers.php
+++ app/Observers/ReplayObservers.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 
+use App\Models\Article;
 use App\Models\Replay;
 
 class ReplayObservers
@@ -14,10 +15,13 @@ class ReplayObservers
     public function deleted(Replay $replay) {
         $article = $replay->article;
 
-        if ($article->replay_count <= 0) {
-            return ;
+        if ($article->replay_count > 0) {
+            $replay->article->decrement('replay_count');
         }
 
-        $replay->article->decrement('replay_count');
+        if ($article = Article::where('answer_id', $replay->id)->first()) {
+            $article->answer_id = 0;
+            $article->save();
+        }
     }
 }
